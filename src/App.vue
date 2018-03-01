0787d6ce8179e5275ccc0a4fade440e147f91c26
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-03-01 15:40:02 -0500

    init logging to loggly

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,15 @@ export default {
         this.addAlert(err)
       }).then(() => {
         this.setFormIsSearching(false)
+        this.log()
+      })
+    },
+    log () {
+      var logUrl = 'http://logs-01.loggly.com/inputs/ff424bed-98df-4ab6-ac0e-49dc5d9ae378/tag/hc-trash-hauler/'
+      this.$http.post(logUrl, JSON.stringify(this.$store.state), {
+        headers: {
+          'Content-Type': 'text/plain'
+        }
       })
     }
   }
